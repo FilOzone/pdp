0eed0cdbde67fe51dfedc6ad3b6efdcf91614fd5
Chaitu-Tatipamula <107246959+Chaitu-Tatipamula@users.noreply.github.com>
2026-03-23 23:02:34 +0530

    chore: add initial PDPVerifierLayout.sol

---
+++ src/PDPVerifierLayout.sol
@@ -0,0 +1,25 @@
+// SPDX-License-Identifier: Apache-2.0 OR MIT
+pragma solidity ^0.8.20;
+
+// Code generated - DO NOT EDIT.
+// This file is a generated binding and any changes will be lost.
+// Generated with tools/generate_storage_layout.sh
+
+bytes32 constant CHALLENGE_FINALITY_SLOT = bytes32(uint256(0));
+bytes32 constant NEXT_DATA_SET_ID_SLOT = bytes32(uint256(1));
+bytes32 constant PIECE_CIDS_SLOT = bytes32(uint256(2));
+bytes32 constant PIECE_LEAF_COUNTS_SLOT = bytes32(uint256(3));
+bytes32 constant SUM_TREE_COUNTS_SLOT = bytes32(uint256(4));
+bytes32 constant NEXT_PIECE_ID_SLOT = bytes32(uint256(5));
+bytes32 constant DATA_SET_LEAF_COUNT_SLOT = bytes32(uint256(6));
+bytes32 constant NEXT_CHALLENGE_EPOCH_SLOT = bytes32(uint256(7));
+bytes32 constant DATA_SET_LISTENER_SLOT = bytes32(uint256(8));
+bytes32 constant CHALLENGE_RANGE_SLOT = bytes32(uint256(9));
+bytes32 constant SCHEDULED_REMOVALS_SLOT = bytes32(uint256(10));
+bytes32 constant SCHEDULED_REMOVALS_BITMAP_SLOT = bytes32(uint256(11));
+bytes32 constant STORAGE_PROVIDER_SLOT = bytes32(uint256(12));
+bytes32 constant DATA_SET_PROPOSED_STORAGE_PROVIDER_SLOT = bytes32(uint256(13));
+bytes32 constant DATA_SET_LAST_PROVEN_EPOCH_SLOT = bytes32(uint256(14));
+bytes32 constant FEE_STATUS_SLOT = bytes32(uint256(15));
+bytes32 constant NEXT_UPGRADE_SLOT = bytes32(uint256(16));
+
