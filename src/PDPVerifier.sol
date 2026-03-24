b67667f3a98d584eadac20721d86e0a1dde4ce07
Abhivansh <31abhivanshj@gmail.com>
2026-03-24 13:20:32 +0530

    fix lint

--- src/PDPVerifier.sol
+++ src/PDPVerifier.sol
@@ -871,9 +871,8 @@ contract PDPVerifier is Initializable, UUPSUpgradeable, OwnableUpgradeable {
 
         address listenerAddr = dataSetListener[setId];
         if (listenerAddr != address(0)) {
-            PDPListener(listenerAddr).nextProvingPeriod(
-                setId, nextChallengeEpoch[setId], dataSetLeafCount[setId], extraData
-            );
+            PDPListener(listenerAddr)
+                .nextProvingPeriod(setId, nextChallengeEpoch[setId], dataSetLeafCount[setId], extraData);
         }
         emit NextProvingPeriod(setId, challengeEpoch, dataSetLeafCount[setId]);
     }
